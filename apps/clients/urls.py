06e796e8fda2bc5c20ec27da3cd9704a76bf41e6
luizrodgs <luizrodgs@gmail.com>
2022-06-23 20:52:23 -0300

    Commit

---
+++ apps/clients/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("<int:client_id>", client_id, name="client_id"),
+    path("buscar", search, name="search"),
+    path("cria/cliente", create_client, name="create_client"),
+    path("deleta/<int:client_id>", delete_client, name="delete_client"),
+    path("edita/<int:client_id>", edit_client, name="edit_client"),
+    path("atualiza_cliente", update_client, name="update_client"),
+]
--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from clients.models import Client
+from django.contrib.auth.models import User
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+
+def index(request):
+    return render (request, 'index.html')
+
+def client_id(request):
+    return render (request, 'index.html')
+
+def search(request):
+    return render (request, 'index.html')
+
+def create_client(request):
+    return render (request, 'index.html')
+
+def delete_client(request):
+    return render (request, 'index.html')
+
+def edit_client(request):
+    return render (request, 'index.html')
+
+def update_client(request):
+    return render (request, 'index.html')
