d0e86c51499732985c8478830181e938293be922
luizrodgs <luizrodgs@gmail.com>
2022-06-23 21:57:35 -0300

    commit

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", client_dashboard, name="client_dashboard"),
     path("<int:client_id>", client_id, name="client_id"),
-    path("buscar", search, name="search"),
+    # path("buscar", search, name="search"),
     path("cria/cliente", create_client, name="create_client"),
     path("deleta/<int:client_id>", delete_client, name="delete_client"),
     path("edita/<int:client_id>", edit_client, name="edit_client"),
--- apps/clients/views.py
+++ apps/clients/views.py
@@ -4,23 +4,49 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 
-def index(request):
-    return render (request, 'index.html')
+def client_dashboard(request):
+    clients = Client.objects.order_by("name")
+    paginator = Paginator(clients, 30)
+    page = request.GET.get('page')
+    clients_per_page = paginator.get_page(page)
+    package = {"clients": clients_per_page}
+    return render (request, 'clients/client_dashboard.html', package)
 
 def client_id(request):
-    return render (request, 'index.html')
+    client = get_object_or_404(Client, pk=id)
+    client_to_show = {"client": client}
+    return render (request, 'clients/client.html', client_to_show)
 
-def search(request):
-    return render (request, 'index.html')
+# def search(request):
+#     return render (request, 'index.html')
 
 def create_client(request):
-    return render (request, 'index.html')
+    if request.method == 'POST':
+        name = request.POST['client_name']
+        phone = request.POST['client_phone']
+        address = request.POST['client_address']
+        client = Client.objects.create(name=name, phone=phone, address=address)
+        client.save()
+        return redirect('client_dashboard')
+    else:
+        return render(request, 'clients/create_client.html')
 
 def delete_client(request):
-    return render (request, 'index.html')
+    client = get_object_or_404(Client, client_id)
+    client.delete()
+    return redirect (request, 'clients/client_dashboard.html')
 
 def edit_client(request):
-    return render (request, 'index.html')
+    client = get_object_or_404(Client, client_id)
+    client_to_edit = {'client': client}
+    return render (request, 'clients/edit_client.html', client_to_edit)
 
 def update_client(request):
-    return render (request, 'index.html')
+    if request.method == 'POST':
+        client_id = request.POST['client_id']
+        client = Client.objects.get(pk=client_id)
+        client.name = request.POST['client_name']
+        client.phone = request.POST['client_phone']
+        client.address = request.POST['client_address']
+        client.save()
+    return redirect (request, 'clients/client_dashboard.html')
