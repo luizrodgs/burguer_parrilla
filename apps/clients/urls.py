56e90f7f72a16d96f4f25339f135d044c527b4e5
Ivan Noleto <ivannoleto@hotmail.com>
2022-06-26 18:36:41 -0300

    url

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", client_dashboard, name="client_dashboard"),
     path("<int:client_id>", get_client, name="client_id"),
     # path("buscar", search, name="search"),
-    path("cria/cliente", create_client, name="create_client"),
-    path("deleta/<int:client_id>", delete_client, name="delete_client"),
-    path("edita/<int:client_id>", edit_client, name="edit_client"),
-    path("atualiza_cliente", update_client, name="update_client"),
+    path("criar", create_client, name="create_client"),
+    path("deletar/<int:client_id>", delete_client, name="delete_client"),
+    path("editar/<int:client_id>", edit_client, name="edit_client"),
+    path("atualizar", update_client, name="update_client"),
 ]
