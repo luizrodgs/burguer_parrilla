0569b387d6cef1917a7848af72b5ebb740becce9
luizrodgs <luizrodgs@gmail.com>
2022-07-28 16:47:44 -0300

    form

--- apps/orders/forms.py
+++ apps/orders/forms.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+
 from clients.models import Client
-from django.forms import CharField, Form, IntegerField, ModelMultipleChoiceField
+from django.forms import CharField, DateTimeField, Form, ModelMultipleChoiceField
 from products.models import Product
 
 from .models import Order
 
 
 class OrderForm(Form):
-    cliente = ModelMultipleChoiceField(queryset=Client.objects.all())
     order = Order.objects.all().last()
-    ord = CharField(label="Número do pedido", disabled=True, initial=order.id + 1)
-    produtos = ModelMultipleChoiceField(queryset=Product.objects.all())
+    ord = CharField(label="Número do pedido:", disabled=True, initial=order.id + 1)
+    date = DateTimeField(label="Data do pedido:", disabled=True, initial=datetime.today)
+    client = ModelMultipleChoiceField(queryset=Client.objects.all(), label="Cliente")
+    products = ModelMultipleChoiceField(
+        queryset=Product.objects.all(), label="Produtos"
+    )
+    obs = CharField(max_length=100)
