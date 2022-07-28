f41868862dff4a64e8b592dbbb6cb2b83d9ced4d
luizrodgs <luizrodgs@gmail.com>
2022-07-28 15:39:37 -0300

    Order id

--- apps/orders/forms.py
+++ apps/orders/forms.py
@@ -1,5 +1,5 @@
 from clients.models import Client
-from django.forms import CharField, Form, ModelMultipleChoiceField
+from django.forms import CharField, Form, IntegerField, ModelMultipleChoiceField
 from products.models import Product
 
 from .models import Order
@@ -11,9 +11,9 @@ class ClientForm(Form):
 
 class OrderForm(Form):
     order = Order.objects.all().last()
-    ord = CharField(label="order", disabled=True, initial=order.id + 1)
+    ord = CharField(label="Número do pedido", disabled=True, initial=order.id + 1)
 
 
 class OrderProductForm(Form):
     order = ModelMultipleChoiceField(queryset=Product.objects.all())
-    quantity = CharField(label="quantity")
+    # quantity = CharField(label="quantity")
