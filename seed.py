117d3bdf6457bcd7450e56fd72c0fda61f7d9fbe
Ivan Noleto <ivannoleto@hotmail.com>
2022-08-04 23:34:38 -0300

    fake address and cellphone

--- seed.py
+++ seed.py
@@ -1,36 +1,24 @@
 import os
 
 import django
+from faker import Faker
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
 django.setup()
 
-import random
-
 from clients.models import Client
-from faker import Faker
 
 
-def creating_clients(quantidade_de_pessoas):
+def creating_clients(number_of_clients):
     fake = Faker("pt_BR")
     Faker.seed(10)
-    for _ in range(quantidade_de_pessoas):
+    for _ in range(number_of_clients):
         name = fake.name()
-        phone = "{}{} 9{}{}{}{}-{}{}{}{}".format(
-            random.randrange(1, 10),
-            random.randrange(1, 10),
-            random.randrange(8, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-            random.randrange(0, 10),
-        )
-        address = ""
-        a = Client(name=name, phone=phone, address=address)
-        a.save()
+        phone = fake.cellphone_number()
+        address = fake.address()
+        client = Client(name=name, phone=phone, address=address)
+        client.save()
 
 
-creating_clients(200)
+if __name__ == "__main__":
+    creating_clients(200)
